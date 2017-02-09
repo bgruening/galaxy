de1381ec48d31a5fa0f09d339aee80ed859d36ec
anuprulez <anup.rulez@gmail.com>
2017-02-09 15:53:00 +0100

    Adding types of representations for ngl viewer

--- config/plugins/visualizations/charts/static/repository/build/ngl_viewer.js
+++ config/plugins/visualizations/charts/static/repository/build/ngl_viewer.js
@@ -48,14 +48,27 @@ define(function() { return /******/ (function(modules) { // webpackBootstrap
 	    return Backbone.Model.extend({
 	        initialize: function( options ) {
 	            var dataset = options.dataset,
+	                settings = options.chart.settings,
 	                url = window.location.protocol + '//' + window.location.host + "/datasets/" + dataset.dataset_id +
 	                      "/display?to_ext=" + dataset.extension;
+	            
 	            Utils.get( {
 	                url     : url,
 	                cache   : true,
 	                success : function( response ) {
-	                    var stage = new ngl.Stage( options.targets[ 0 ] );
-	                    stage.loadFile( url, {ext: dataset.extension, name: dataset.name, defaultRepresentation: true} );
+	                    var viewer_options = {};
+	                    _.each( settings.attributes, function( value, key ) {
+	                        if ( key.startsWith( 'viewer|' ) ) {
+	                            viewer_options[ key.replace( 'viewer|', '' ) ] = value;
+	                        }
+	                    });
+	
+	                    var stage = new ngl.Stage( options.targets[ 0 ], { backgroundColor: settings.get( 'backcolor' ) } );
+	                    stage.loadFile( url, {ext: dataset.extension, name: dataset.name, defaultRepresentation: true} ).then( function( o ) {
+	                        o.addRepresentation( viewer_options.mode, { radius: viewer_options.radius } );
+	                        o.centerView();
+	                    });
+	                    stage.setQuality( settings.get( 'quality' ) );
 	                    options.chart.state( 'ok', 'Chart drawn.' );
 	                    options.process.resolve();
 	                },
--- config/plugins/visualizations/charts/static/repository/visualizations/ngl/viewer/config.js
+++ config/plugins/visualizations/charts/static/repository/visualizations/ngl/viewer/config.js
@@ -10,8 +10,16 @@ define( [], function() {
                 help    : 'Select the rendering quality.',
                 type    : 'select',
                 display : 'radio',
-                value   : 'medium',
+                value   : 'light',
                 data    : [ { label : 'High', value : 'high' }, { label : 'Medium', value : 'medium' }, { label : 'Low', value : 'low' } ]
+            }, 
+            backcolor : {
+                label   : 'Background Color',
+                help    : 'Select background color of the viewer.',
+                type    : 'select',
+                display : 'radio',
+                value   : 'white',
+                data    : [ { label : 'Light', value : 'white' }, { label : 'Dark', value : 'black' } ]
             },
             viewer : {
                 type        : 'conditional',
@@ -23,73 +31,12 @@ define( [], function() {
                     value   : 'cartoon',
                     help    : 'Select the rendering mode.',
                     data    : [ { label : 'Cartoon',        value : 'cartoon' },
-                                { label : 'Lines',          value : 'lines' },
-                                { label : 'Points',         value : 'points' },
-                                { label : 'Spheres',        value : 'spheres' },
-                                { label : 'Trace',          value : 'trace' },
-                                { label : 'Trace (line)',   value : 'lineTrace' },
-                                { label : 'Trace (smooth)', value : 'sline' },
-                                { label : 'Tube',           value : 'tube' } ]
+                                { label : 'Line',          value : 'line' } ]
                 },
                 cases       : [ { value : 'cartoon', inputs: [ {
                                     name  : 'radius',
                                     label : 'Radius',
                                     help  : 'Radius of tube profile. Also influences the profile thickness for helix and strand profiles.',
-                                    type  : 'float',
-                                    min   : 0.1,
-                                    max   : 3,
-                                    value : 0.3
-                                } ] },
-                                { value : 'lines', inputs: [ {
-                                    name  : 'lineWidth',
-                                    label : 'Line width',
-                                    help  : 'Specify the line width.',
-                                    type  : 'float',
-                                    min   : 0.1,
-                                    max   : 10,
-                                    value : 4
-                                } ] },
-                                { value : 'points', inputs: [ {
-                                    name  : 'pointSize',
-                                    label : 'Point size',
-                                    help  : 'Specify the point size.',
-                                    type  : 'float',
-                                    min   : 0.1,
-                                    max   : 10,
-                                    value : 1
-                                } ] },
-                                { value : 'spheres' },
-                                { value : 'trace', inputs: [ {
-                                    name  : 'radius',
-                                    label : 'Radius',
-                                    help  : 'Specify the tube radius.',
-                                    type  : 'float',
-                                    min   : 0.1,
-                                    max   : 3,
-                                    value : 0.3
-                                } ] },
-                                { value : 'lineTrace', inputs: [ {
-                                    name  : 'lineWidth',
-                                    label : 'Line width',
-                                    help  : 'Specify the line width.',
-                                    type  : 'float',
-                                    min   : 0.1,
-                                    max   : 10,
-                                    value : 4
-                                } ] },
-                                { value : 'sline', inputs: [ {
-                                    name  : 'lineWidth',
-                                    label : 'Line width',
-                                    help  : 'Specify the line width.',
-                                    type  : 'float',
-                                    min   : 0.1,
-                                    max   : 10,
-                                    value : 4
-                                } ] },
-                                { value : 'tube', inputs: [ {
-                                    name  : 'radius',
-                                    label : 'Radius',
-                                    help  : 'Specify the tube radius.',
                                     type  : 'float',
                                     min   : 0.1,
                                     max   : 3,
